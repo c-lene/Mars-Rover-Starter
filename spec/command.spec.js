e5a8e4c27de05bd29b9abb525d8714f95a5aeeb1
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-21 23:21:04 -0500

    completed 3/13 test

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,8 +5,23 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
+  //Test 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+   //Test 2
+   test("constructor sets command type", function() {
+    let commandType;
+    expect(this.commandType).toBe(commandType);
+  });  
+
+  
+  //Test 3
+  test("constructor sets a value passed in as the 2nd argument", function() {
+    let value;
+    expect(this.value).toBe(value);
+  });
+  
 });
+
