93afd68b4d62defee9ed238e58fc4fbb33a1e59e
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-24 00:02:17 -0500

    still working on

--- rover.js
+++ rover.js
@@ -1,5 +1,31 @@
+const Message = require('./message.js');
+const Command = require('./command.js');
+
 class Rover {
    // Write code here!
+   constructor(position) {
+      this.position = position;
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+   // STILL WORKING - Confused on how to call imported object properties as a value
+   recieveMessage(message) {                                   //'message' parameter should be a 'Message' object         
+      let messageInfo = {                               // Declared & initialized a new object to store the message & results properties
+         message: message.name,
+         results: message.command,
+      }
+      return messageInfo;                               // receiveMessage() should return data from 'messageInfo' object
+   };
 }
+
+// let commands = [new Command('MODE_CHANGE', 'LOW-POWER'), new Command('STATUS_CHECK')];
+// let message = new Message('Test message with two commands', commands);
+// let rover = new Rover(98382);                   // Passes 98382 as the rover's position.
+
+// let response = rover.recieveMessage(message);
+
+// console.log(response);
+
+
 
 module.exports = Rover;
