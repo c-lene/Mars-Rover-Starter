4c80d17dcf8f55510a9b730cec71082dca7f7b4f
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-24 23:46:57 -0500

    CT updated completed test 1-3

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -6,20 +6,20 @@ const Command = require('../command.js');
 describe("Command class", function() {
 
   
-  //Test 1
+  // Test 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
 
-   //Test 2
+   // Test 2
    test("constructor sets command type", function() {
     let modeCommand = new Command('MODE_CHANGE', 'LOW_POWER');
     expect(modeCommand.commandType).toBe('MODE_CHANGE', 'LOW_POWER');
   });  
 
   
-  //Test 3
+  // Test 3
   test("constructor sets a value passed in as the 2nd argument", function() {
     let moveCommand = new Command('MOVE', 12000);
     expect(moveCommand.value).toBe(12000);
